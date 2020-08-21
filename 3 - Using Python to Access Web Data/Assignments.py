ce7dbd36840c86f0c73da0b2f5e8a3cd945d56b2
Cristian Yamamoto <yamamotocristian@gmail.com>
2020-08-21 14:57:42 -0300

    Adding and updating files from 3rd module of Python for Everybody specialization

--- 3 - Using Python to Access Web Data/Assignments.py
+++ 3 - Using Python to Access Web Data/Assignments.py
@@ -141,3 +141,103 @@
 
 #     # print('lat', lat, 'lng', lng)
 #     # print(location)
+################################################################################
+# # Assignment: Extracting Data from JSON
+# import urllib.request, urllib.parse, urllib.error
+# import ssl
+# import json
+
+# # Ignore SSL certificate errors
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
+
+# url = input('Enter location: ')
+# # url = 'http://py4e-data.dr-chuck.net/comments_42.json' #sample data
+# # url = 'http://py4e-data.dr-chuck.net/comments_907679.json'
+
+# print('Retrieving', url)
+# connection = urllib.request.urlopen(url, context=ctx)
+# data = connection.read()
+# print('Retrieved', len(data), 'characters')
+# data = data.decode()
+# # print(data)
+
+# # data = '''
+# # [
+# #   { "id" : "001",
+# #     "x" : "2",
+# #     "name" : "Chuck"
+# #   } ,
+# #   { "id" : "009",
+# #     "x" : "7",
+# #     "name" : "Brent"
+# #   }
+# # ]'''
+
+# info = json.loads(data)
+# print('User count:', len(info["comments"]))
+
+# soma = 0 
+# for item in info["comments"]:
+#     # print('Name', item['name'])
+#     # print('Id', item['id'])
+#     # print('Attribute', item['x'])
+#     soma += item['count']
+
+# print('Sum: ', soma)
+################################################################################
+# Assignment: Using the GeoJSON API
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    # print(json.dumps(js, indent=4))
+
+    # lat = js['results'][0]['geometry']['location']['lat']
+    # lng = js['results'][0]['geometry']['location']['lng']
+    # print('lat', lat, 'lng', lng)
+    # location = js['results'][0]['formatted_address']
+    # print(location)
+    place_id = js['results'][0]['place_id']
+    print('Place id', place_id)
